8dfb2fd34424e07ce77835c40857da7a8f57c3f1
MasterPorti <masterportilol@gmail.com>
2023-05-04 18:28:58 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { ref,computed } from 'vue';
+
+const styleRed = "color: red;";
+
+const styleGreen = "color: green;";
+
+
+const total = ref(0)
+const fav = ref([])
+
+const classTotal = computed(()=>{
+  if (total.value <= 0){
+    return 'red'
+  }else{
+    return 'green'
+  }
+})
+
+
+const disableButton = computed(()=>{
+  if(fav.value.find(item => item === total.value.toString())){
+    return true
+  }else{
+    return false
+  }
+})
+
+const handleSumar = () => {
+  total.value++
+}
+
+
+const handleRestar = () => {
+  total.value--
+}
+
+const handleFavorios = () => {
+
+    fav.value.push(total.value.toString())
+
+}
+
+</script>
+
+
+
+<template>
+  <button @click="handleSumar">
+    Sumar
+  </button>
+  <button @click="handleRestar">
+    Restar
+  </button>
+  <button @click="handleFavorios" :disabled="disableButton">
+    Agregar a Favoritos
+  </button>
+  <p :class="classTotal">Total {{ total }}</p>
+
+  <template v-for="numero in fav">
+    <p>{{ numero }}</p>
+  </template>
+  
+</template>
+
+
+<style>
+.green {
+  background-color: green;
+}
+
+.red{
+  background-color: red;
+}
+
+</style>
